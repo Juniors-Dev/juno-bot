4fe5bd6fb4ad9919674edeea43296c19b031de31
Anclagen <91462616+Anclagen@users.noreply.github.com>
2025-10-20 12:20:35 +0200

    feat: updated flag and divide form create and handler

--- commands/users/create-user.js
+++ commands/users/create-user.js
@@ -1,4 +1,10 @@
-import { SlashCommandBuilder, ActionRowBuilder, ButtonStyle, ButtonBuilder } from "discord.js";
+import {
+  SlashCommandBuilder,
+  ActionRowBuilder,
+  ButtonStyle,
+  ButtonBuilder,
+  MessageFlags,
+} from "discord.js";
 
 export default {
   data: new SlashCommandBuilder()
@@ -6,13 +12,12 @@ export default {
     .setDescription("Create new user in database"),
 
   async execute(interaction) {
-    console.log("creating form");
-    await interaction.deferReply({ ephemeral: true }); // acknowledges immediately
+    await interaction.deferReply({ flags: MessageFlags.Ephemeral }); // acknowledges immediately
     try {
       const { userService } = interaction.services;
       const isUser = await userService.getOneDiscordId(interaction.user.id);
       if (isUser) {
-        return interaction.editReply({ content: "User already exists.", ephemeral: true });
+        return interaction.editReply({ content: "User already exists." });
       }
 
       const button = new ButtonBuilder()
@@ -25,11 +30,11 @@ export default {
       return interaction.editReply({
         content: "You’re not in the database yet. Click below to create a profile.",
         components: [row],
-        ephemeral: true,
+        flags: MessageFlags.Ephemeral,
       });
     } catch (err) {
       console.log(err);
-      await interaction.editReply({ content: "Oops and error occurred", ephemeral: true });
+      await interaction.editReply({ content: "Oops and error occurred" });
     }
   },
 };
--- events/users/create-user-modal.js
+++ events/users/create-user-modal.js
@@ -4,34 +4,14 @@ import {
   TextInputBuilder,
   TextInputStyle,
   ActionRowBuilder,
+  MessageFlags,
 } from "discord.js";
 
 export default {
   name: Events.InteractionCreate,
   async execute(interaction) {
     // --- handle button ---
-    if (interaction.isModalSubmit() && interaction.customId === "create_user_modal") {
-      const { userService } = interaction.services;
-
-      const name = interaction.fields.getTextInputValue("name").trim();
-      const gh = interaction.fields.getTextInputValue("gh").trim();
-
-      await interaction.deferReply({ ephemeral: true });
-
-      const existing = await userService.getOneDiscordId(interaction.user.id);
-
-      if (existing) {
-        return interaction.editReply("User already exists.");
-      }
-
-      await userService.create({
-        discordId: interaction.user.id,
-        githubUsername: gh || null,
-        name,
-      });
-
-      await interaction.editReply("✅ User created successfully.");
-    } else if (interaction.customId === "create_user_modal_button") {
+    if (interaction.customId === "create_user_modal_button") {
       console.log("form time!");
       // Build the modal
       const modal = new ModalBuilder().setCustomId("create_user_modal").setTitle("Create User");
