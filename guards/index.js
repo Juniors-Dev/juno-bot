824bf0c97628901b728d008ebaf5b25ae8a21774
fmSand <fridamsand@gmail.com>
2025-11-02 18:32:02 +0100

    refactor: simplify guard exports using re-export pattern

--- guards/index.js
+++ guards/index.js
@@ -1,9 +1,2 @@
-import { requireActiveSession } from "./requireActiveSession.js";
-import { requireNoActiveSession } from "./requireNoActiveSession.js";
-
-export const guardMap = {
-  activeSession: requireActiveSession,
-  noSession: requireNoActiveSession,
-};
-
-export { requireActiveSession, requireNoActiveSession };
+export * from "./requireActiveSession.js";
+export * from "./requireNoActiveSession.js";
