2045f48cad868491fccc18aabdaf356f38f65429
fmSand <fridamsand@gmail.com>
2025-11-03 14:25:57 +0100

    refactor(guards): read session from interaction.context

--- guards/requireActiveSession.js
+++ guards/requireActiveSession.js
@@ -1,7 +1,7 @@
 import { replyEphemeral } from "../utils/reply.js";
 
 export async function requireActiveSession(interaction) {
-  if (interaction.activeSession) return true;
+  if (interaction.context?.session) return true;
   //TODO: Add clock in button
   await replyEphemeral(interaction, "You're not clocked in. Use `/clock-in` first.");
   return false;
--- guards/requireNoActiveSession.js
+++ guards/requireNoActiveSession.js
@@ -1,7 +1,7 @@
 import { replyEphemeral } from "../utils/reply.js";
 
 export async function requireNoActiveSession(interaction) {
-  if (!interaction.activeSession) return true;
+  if (!interaction.context?.session) return true;
   //TODO: Add clock out button
   await replyEphemeral(interaction, "You're already clocked in. Use `/clock-out` first.");
   return false;
