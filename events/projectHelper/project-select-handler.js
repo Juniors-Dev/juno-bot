931c2db5b40fad7fa194335806a779eb2ab108ef
Anclagen <91462616+Anclagen@users.noreply.github.com>
2025-11-14 00:23:20 +0100

    fix: removed console log and removed space on env example

---
+++ events/projectHelper/project-select-handler.js
@@ -0,0 +1,24 @@
+import {
+  Events,
+  ActionRowBuilder,
+  StringSelectMenuBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  EmbedBuilder,
+} from "discord.js";
+import { renderProjectManager } from "../../utils/renderProjectManager.js";
+
+export default {
+  name: Events.InteractionCreate,
+  async execute(interaction) {
+    console.log(interaction);
+    if (!interaction.isStringSelectMenu()) return;
+    if (interaction.customId !== "project_select") return;
+    const selectedId = interaction.values[0];
+    const { projectService, userService } = interaction.services;
+    const user = await userService.getOneDiscordId(interaction.user.id);
+    const projects = await projectService.listByUser(user.id);
+    const { content, components } = renderProjectManager(projects, selectedId);
+    await interaction.update({ content, components });
+  },
+};
