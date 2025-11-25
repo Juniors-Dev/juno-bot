26240592b87664f6b77c4e295a6835b63c77b795
Frida <163577510+fmSand@users.noreply.github.com>
2025-11-25 07:27:05 +0100

    Update requireActiveSession.js
    context -> botContext

--- guards/requireActiveSession.js
+++ guards/requireActiveSession.js
@@ -1,7 +1,7 @@
 import { replyEphemeral } from "../utils/reply.js";
 
 export async function requireActiveSession(interaction) {
-  if (interaction.context?.session) return true;
+  if (interaction.botContext?.session) return true;
   //TODO: Add clock in button
   await replyEphemeral(interaction, "You're not clocked in. Use `/clock-in` first.");
   return false;
