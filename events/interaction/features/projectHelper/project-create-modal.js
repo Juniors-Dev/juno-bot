5a1675f1b8d1d0adc2f0b27d974c0f995a47e27b
Anclagen <91462616+Anclagen@users.noreply.github.com>
2025-11-26 12:06:49 +0100

    fix: show updated dashboard on project creation

--- events/interaction/features/projectHelper/project-create-modal.js
+++ events/interaction/features/projectHelper/project-create-modal.js
@@ -1,9 +1,9 @@
 import { ModalBuilder, TextInputBuilder, TextInputStyle, ActionRowBuilder } from "discord.js";
 
 export async function projectCreateModal(interaction) {
-  console.log(interaction.customId, interaction.botContext);
   if (interaction.customId !== "project_create") return;
   if (!interaction.botContext.user) return;
+
   const modal = new ModalBuilder()
     .setCustomId("create_project_modal")
     .setTitle("Create New Project");
