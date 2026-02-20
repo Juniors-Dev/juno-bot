3bfdb1b4f47d3b4c1c5a86a4b180332483a8f85c
fmSand <fridamsand@gmail.com>
2026-02-20 01:03:52 +0100

    feat: implement handleMyDashboardSelects for month stats retrieval

--- events/interaction/handlers/select.js
+++ events/interaction/handlers/select.js
@@ -1,5 +1,7 @@
 import { projectSelectHandler } from "../features/projectHelper/project-select-menu-handler.js";
 import { handleClockInSelects } from "../features/sessions/handle-clock-in-interactions.js";
+import { handleMyDashboardSelects } from "../features/sessions/handle-my-dashboard-select.js";
+import { handleTaskSelects } from "../features/tasks/handlers/selects/index.js";
 
 export const selectHandlers = {
   project_select: {
@@ -8,6 +10,14 @@ export const selectHandlers = {
   },
   clock_in: {
     run: handleClockInSelects,
+    context: { needsUser: true, needsSession: false },
+  },
+  my_dashboard: {
+    run: handleMyDashboardSelects,
+    context: { needsUser: true, needsSession: false },
+  },
+  tasks: {
+    run: handleTaskSelects,
     context: { needsUser: true, needsSession: false },
   },
 };
