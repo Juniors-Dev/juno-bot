6f2374b0813531b783b295911792ea7e31d0d43a
fmSand <fridamsand@gmail.com>
2026-05-13 13:29:41 +0200

    feat: implement caching for getOneDiscordId method using LRUCache

--- services/UserService.js
+++ services/UserService.js
@@ -1,3 +1,7 @@
+import { LRUCache } from "lru-cache";
+
+const userCache = new LRUCache({ max: 500, ttl: 1000 * 60 * 15 });
+
 export default class UserService {
   constructor(db) {
     this.client = db.sequelize;
@@ -9,9 +13,11 @@ export default class UserService {
   }
 
   async getOneDiscordId(discordId) {
-    return await this.User.findOne({
-      where: { discordId },
-    });
+    const cached = userCache.get(discordId);
+    if (cached !== undefined) return cached;
+    const user = await this.User.findOne({ where: { discordId } });
+    if (user) userCache.set(discordId, user);
+    return user;
   }
 
   async getOneId(id) {
